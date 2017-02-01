439f12c20584de55298ef153f743a0add00706ec
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-02-01 15:54:27 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,7 +4,7 @@ require 'pry'
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
+  #     email: "jon_snow@thewall.we",
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
@@ -13,7 +13,12 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
-def remove_strawberry(contacts)
 
+def remove_strawberry(contacts)
+  #If only there was a way to get a value by its key
+  contacts.each do |name, person|
+    person.each do |key, val|
+      val.delete("strawberry") if key==:favorite_ice_cream_flavors
+    end if name=="Freddy Mercury"
+  end
 end
